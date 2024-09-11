7654a9d227fbefbf5500a4a65d4c3ad98d6edbf8
willt001 <115313153+willt001@users.noreply.github.com>
2024-09-11 13:12:02 +0100

    Simplify headers for GET request

--- dags/chess_etl_code.py
+++ dags/chess_etl_code.py
@@ -20,17 +20,7 @@ def get_monthly_games(date, username='willlt001'):
     year_month = str(date)[:7].replace('-', '/')
     url = f'https://api.chess.com/pub/player/{username}/games/{year_month}'
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Cache-Control": "max-age=0",
+        "User-Agent": username
     }
     r = requests.get(url, headers=headers)
     if r.status_code != 200:
