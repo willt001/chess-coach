48a52cd843402ea6c40734a2a6319f33c599b2d4
Thomas <twill20129@gmail.com>
2024-10-10 00:40:16 +0100

    Add Glue Crawler Task

--- dags/chess_dag_lambda.py
+++ dags/chess_dag_lambda.py
@@ -1,10 +1,18 @@
 import pendulum
+import boto3
 from airflow.decorators import dag, task, task_group
 from extract_games import get_monthly_games
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+from airflow.providers.amazon.aws.operators.glue_crawler import GlueCrawlerOperator
 import logging
-from config import BUCKET_NAME, LAMBDA_FUNCTION_NAME
+from config import BUCKET_NAME, LAMBDA_FUNCTION_NAME, GLUE_CRAWLER_CONFIG, REGION_NAME
+import json
+from airflow.exceptions import AirflowSkipException
+from airflow.hooks.base import BaseHook
+import time
+
+glue_crawler_config = json.loads(GLUE_CRAWLER_CONFIG)
 
 @dag(
     start_date=pendulum.datetime(2019, 1, 1, tz='UTC'),
@@ -27,8 +35,11 @@ def chess_etl_lambda():
             logging.info(zipped_kwargs['payload'])
             return zipped_kwargs
         
-        kwargs = process_xcom(zipped_kwargs)
+        @task.bash(trigger_rule='all_done')
+        def delete_local_file(filename):
+            return f'rm -rf /opt/airflow/{filename}'    
         
+        kwargs = process_xcom(zipped_kwargs)
         local_to_s3 = LocalFilesystemToS3Operator(
                                                 task_id='local_to_s3',
                                                 filename=kwargs['filename'],
@@ -37,11 +48,6 @@ def chess_etl_lambda():
                                                 aws_conn_id='aws_access',
                                                 replace=True
         )
-
-        @task.bash(trigger_rule='all_done')
-        def delete_local_file(filename):
-            return f'rm -rf /opt/airflow/{filename}'                                        
-        
         calculate_blunders = LambdaInvokeFunctionOperator(
                                                     task_id='calculate_blunders',
                                                     function_name=LAMBDA_FUNCTION_NAME,
@@ -51,8 +57,33 @@ def chess_etl_lambda():
         )
         
         local_to_s3 >> [calculate_blunders, delete_local_file(kwargs['filename'])]
+    
+    @task(pool='glue_crawler_pool')
+    def wait_30_seconds():
+        time.sleep(30)
+
+    @task
+    def check_glue_crawler_status(crawler_name):
+        connection = BaseHook.get_connection('aws_access')
+        glue_client = boto3.client(
+                                'glue',
+                                aws_access_key_id=connection.login,
+                                aws_secret_access_key=connection.password,
+                                region_name=REGION_NAME
+                                )
+        response = glue_client.get_crawler(Name=crawler_name)
+        crawler_state = response['Crawler']['State']
+        if crawler_state in ['RUNNING', 'STOPPING']:
+            raise AirflowSkipException(f"Crawler {crawler_name} is already running.")
+    
+    crawl_s3 = GlueCrawlerOperator(
+        task_id="crawl_s3",
+        config=glue_crawler_config,
+        aws_conn_id='aws_access',
+        wait_for_completion=False
+    )
 
     data = extract_games()
-    process_partition.expand(zipped_kwargs=data)
+    process_partition.expand(zipped_kwargs=data) >> wait_30_seconds() >> check_glue_crawler_status(glue_crawler_config['Name']) >> crawl_s3
 
 chess_etl_lambda()
--- dags/config.py
+++ dags/config.py
@@ -1,2 +1,12 @@
+import json
+
+
 BUCKET_NAME = "chess-coach-041"
 LAMBDA_FUNCTION_NAME = 'calculate-blunders'
+GLUE_CRAWLER_CONFIG = json.dumps({
+        "Name": "chess-coach-crawler",
+        "Role": "arn:aws:iam::905418173427:role/AirflowChessCoachCrawler",
+        "DatabaseName": "chess-coach",
+        "Targets": {"S3Targets": [{"Path": "chess-coach-041"}]},
+    })
+REGION_NAME = "eu-north-1"
